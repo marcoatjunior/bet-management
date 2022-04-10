77f78662a7f0e92b4ad05f9d08e673edf41db0cd
Marco Taborda <marco-junior@procergs.rs.gov.br>
2022-04-10 14:46:40 -0300

    refac: Altera contexto de 'times de futebol' para 'times'

--- src/main/java/com/matj/bet/management/api/rest/team/TeamRest.java
+++ src/main/java/com/matj/bet/management/api/rest/team/TeamRest.java
@@ -3,7 +3,7 @@ package com.matj.bet.management.api.rest.team;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Tag(name = "Times de Futebol")
+@Tag(name = "Times")
 @RequestMapping("/teams")
 public interface TeamRest {
 }
