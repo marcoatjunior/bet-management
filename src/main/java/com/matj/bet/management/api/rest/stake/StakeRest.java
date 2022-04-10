771d209b429e0b41387493dd398915bb2c05c928
Marco Taborda <marco-junior@procergs.rs.gov.br>
2022-04-10 14:46:09 -0300

    refac: Altera contexto de 'unidades de medida' para 'unidades'

--- src/main/java/com/matj/bet/management/api/rest/stake/StakeRest.java
+++ src/main/java/com/matj/bet/management/api/rest/stake/StakeRest.java
@@ -3,7 +3,7 @@ package com.matj.bet.management.api.rest.stake;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Tag(name = "Unidades de medida")
+@Tag(name = "Unidades")
 @RequestMapping("/stakes")
 public interface StakeRest {
   
