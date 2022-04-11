3e7f69c49ba6204eedbeaac1273bff23b5eda815
Marco Taborda <marco-junior@procergs.rs.gov.br>
2022-04-11 19:10:00 -0300

    fix: Ajusta nomenclatura do parâmetro de entrada da consulta de método por nome

--- src/main/java/com/matj/bet/management/api/repository/method/MethodRepository.java
+++ src/main/java/com/matj/bet/management/api/repository/method/MethodRepository.java
@@ -8,5 +8,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface MethodRepository extends MongoRepository<MethodEntity, String> {
 
-  Optional<MethodEntity> findByName(String size);
+  Optional<MethodEntity> findByName(String name);
 }
